39c3f5409bb12b287734fc5b5c5a7b3594e66c34
Serhiy Hasiuk <Serhiy.Hasiuk@symphony-solutions.eu>
2016-02-09 18:52:48 +0200

    3d commit [1]

--- src/js/widget.js
+++ src/js/widget.js
@@ -2,7 +2,7 @@
 
 var Widget = (function() {
   
-  const css = {
+  const CSS = {
     "w": "widget",
     "w--collapsed": "widget_collapsed",
     "w_head": "widget__header",
@@ -12,11 +12,11 @@ var Widget = (function() {
     "w_body": "widget__body",
     "w_footer": "widget__footer",
     "w-sum": "widget-sum",
-    "w-sum_title": "widget-sum__title",
+    "w-sum_text": "widget-sum__text",
     "w-sum_val": "widget-sum__value"
   };
   
-  class Widget {
+  class Widget {    
     constructor(container = document.body, title = "Expenses") {
       this.container = container;
       this.title = title;
@@ -27,35 +27,95 @@ var Widget = (function() {
       ];      
      
       this._render();
-      this._widget = this.container.querySelector(`.${css['w']}`);
-      this._expandBtn = this.container.querySelector(`.${css['w_expand']}`);
+      this._widget = this.container.querySelector(`.${CSS['w']}`);
+      this._expandBtn = this.container.querySelector(`.${CSS['w_expand']}`);
 
       this._expandBtn.addEventListener('click', this.expandWidget.bind(this), false);
     }
     
-    _calculateAmmount() {
+    _calculateAmount() {
       return this._data.reduce((prev, curr) => {
         return prev + (curr.value || 0);
       }, 0);
     }
     
     _generateTemplate() {
-      let ammount = this._calculateAmmount();
+      let amount = this._calculateAmount();
       
       let template =
-        `<div class="${css['w']}">
-        <header class="${css['w_head']}">
-          <h3 class="${css['w_title']}">${this.title}</h3>
-          <div class="${css['w_expand']}"></div>
+        `<div class="${CSS['w']}">
+        <header class="${CSS['w_head']}">
+          <h3 class="${CSS['w_title']}">${this.title}</h3>
+          <div class="${CSS['w_expand']}"></div>
         </header>
-        <div class="${css['w_body']}">
-          <footer class="${css['w_footer']}">
-            <div class="${css['w-sum']}">
-              <div class="${css['w-sum_title']}">Total:</div>
-              <div class="${css['w-sum_val']}">${ammount}</div>
+        <section class="widget__body">
+          <div class="widget-table">
+            <div class="widget-table__head">
+              <div class="widget-table__tr">
+                <div class="widget-table__td">
+                  <div class="widget-table__head-item">Name</div>
+                </div>
+                <div class="widget-table__td">
+                  <div class="widget-table__head-item">Amount</div>
+                </div>
+              </div>
             </div>
-          </footer>
-        </div>
+            <div class="widget-table__body">
+              <div class="widget-table__tr">
+                <div class="widget-table__td"><span>1</span></div>
+                <div class="widget-table__td"><span>2</span></div>
+              </div>
+                            <div class="widget-table__tr">
+                <div class="widget-table__td"><span>1</span></div>
+                <div class="widget-table__td"><span>2</span></div>
+              </div>
+                            <div class="widget-table__tr">
+                <div class="widget-table__td"><span>1</span></div>
+                <div class="widget-table__td"><span>2</span></div>
+              </div>
+                            <div class="widget-table__tr">
+                <div class="widget-table__td"><span>1</span></div>
+                <div class="widget-table__td"><span>2</span></div>
+              </div>
+                            <div class="widget-table__tr">
+                <div class="widget-table__td"><span>1</span></div>
+                <div class="widget-table__td"><span>2</span></div>
+              </div>
+                            <div class="widget-table__tr">
+                <div class="widget-table__td"><span>1</span></div>
+                <div class="widget-table__td"><span>2</span></div>
+              </div>
+                            <div class="widget-table__tr">
+                <div class="widget-table__td"><span>1</span></div>
+                <div class="widget-table__td"><span>2</span></div>
+              </div>
+                            <div class="widget-table__tr">
+                <div class="widget-table__td"><span>1</span></div>
+                <div class="widget-table__td"><span>2</span></div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer class="${CSS['w_footer']}">
+          <div class="widget-form">
+            <div class="widget-form__cell">
+              <input class="widget-form__input" 
+                    type="text"
+                    name="w_name"/>
+            </div>
+            <div class="widget-form__cell">
+              <span class="widget-form__symbol"></span>
+              <input class="widget-form__input widget-form__input_val" 
+                    type="text"
+                    name="w_value"/>
+              <button class="widget-form__submit" name="w_submit"></button>
+            </div>
+          </div>
+          <div class="widget-sum">
+            <div class="widget-sum__text">Total</div>           
+            <div class="widget-sum__value">${amount}</div>
+          </div>
+        </footer>
       </div>`;
 
       return template;
@@ -66,14 +126,15 @@ var Widget = (function() {
     }
     
     expandWidget(e) {
-      if (this._widget.classList.contains(css['w--collapsed'])) {
-        this._widget.classList.remove(css['w--collapsed']);
-        this._expandBtn.classList.remove(css['w_expand--active']);
+      if (this._widget.classList.contains(CSS['w--collapsed'])) {
+        this._widget.classList.remove(CSS['w--collapsed']);
+        this._expandBtn.classList.remove(CSS['w_expand--active']);
+        
         return false;
       }
 
-      this._widget.classList.add(css['w--collapsed']);
-      this._expandBtn.classList.add(css['w_expand--active']);
+      this._widget.classList.add(CSS['w--collapsed']);
+      this._expandBtn.classList.add(CSS['w_expand--active']);
     }
     
     getData() {
@@ -82,5 +143,5 @@ var Widget = (function() {
   }
   
   return Widget;
-})();
+}());
 
